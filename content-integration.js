2fbdfc70ac6c4be60b0365d8f5e900525e06b8e5
miguel.cerqueira <miguel.cerqueira@netexlearning.com>
2019-10-22 13:02:24 +0200

    Primera aproximación a la prueba de concepto de descargas:
    Se han hecho mejoras de interfaz y ya descarga correctamente.

--- content-integration.js
+++ content-integration.js
@@ -1,8 +1,14 @@
 const {ipcRenderer} = require('electron');
 const axios = require('axios').default;
 const fs = require('fs');
+const stringify = require('querystring').stringify;
+
+ipcRenderer.on('log' , function(event , data){
+    log(data);
+});
 
 const downloadResources = (form) => {
+    clearLog();
     try {
         const token = form.token.value;
         const payload = JSON.parse(form.payload.value);
@@ -14,15 +20,20 @@ const downloadResources = (form) => {
                 Authorization: `Bearer ${token}`
             }
         }).then(response => {
-            log("JSON de descargas recuperado correctamente.");
-            response.data.map(resourceInfo => {
-                if (resourceInfo.launcher) {
-                    copyToLocalDb(resourceInfo);
-                }
-                if (resourceInfo.url) {
-                    storeResouceOnDisk(resourceInfo);
-                }
-            });
+            if (response.data.length) {
+                log("JSON de descargas recuperado correctamente.");
+                response.data.map(resourceInfo => {
+                    if (resourceInfo.launcher) {
+                        copyToLocalDb(resourceInfo);
+                        printResource(resourceInfo.id);
+                    }
+                    if (resourceInfo.url) {
+                        storeResouceOnDisk(resourceInfo);
+                    }
+                });
+            } else {
+                log("No se ha encontrado el recurso.");
+            }
         }).catch(error => {
             log(error);
         });
@@ -41,6 +52,7 @@ const storeResouceOnDisk = (resourceInfo) => {
     const folder = `${__dirname}/resources/${resourceInfo.id}/`;
     createFolder(folder);
     ipcRenderer.send('download', resourceInfo.url, folder);
+    log(`Descargando ${resourceInfo.url}`);
 };
 
 function createFolder(folder) {
@@ -53,5 +65,42 @@ function createFolder(folder) {
 
 const log = (text) => {
     const logArea = document.getElementById("logArea");
-    logArea.innerText = logArea.innerText + text;
+    logArea.innerHTML = logArea.innerHTML + text + "\n";
+    logArea.scrollTop = logArea.scrollHeight;
 };
+
+const clearLog = () => {
+    const logArea = document.getElementById("logArea");
+    logArea.innerHTML = "";
+};
+
+const printResource = (key) => {
+    const resourceUrl = localStorage.getItem(key);
+    const resourceElement = document.createElement('a');
+    const linkText = document.createTextNode(key);
+    resourceElement.appendChild(linkText);
+    resourceElement.title = key;
+    resourceElement.href = resourceUrl;
+    document.getElementById("resourceList").appendChild(resourceElement);
+};
+
+const getToken = () => {
+    axios({
+        url: 'http://dev-auth.smarted.cloud/api/v1/oauth/token',
+        method: 'POST',
+        data: stringify({
+            'grant_type': 'trusted_client_user_id',
+            'user_id': 'ad169e09-a79e-401c-b100-4a5cc7167e0e'
+        }),
+        headers: {
+            Authorization: `Basic c21hcnRlZC14dW50YTphZ3JlZWQtc2V0dGxlZC1NT05FWS1uZXJ2ZQ==`
+        }
+    }).then(response => {
+        document.getElementById('token').value = response.data.access_token;
+        log("Token actualizado.");
+    }).catch(error => {
+        log(`Error al obtener el token: ${error}`);
+    });
+};
+
+getToken();
--- index.js
+++ index.js
@@ -1,31 +1,35 @@
-const { ipcMain, app, BrowserWindow } = require('electron');
+const {ipcMain, app, BrowserWindow} = require('electron');
 const {download} = require('electron-dl');
 
 let win;
 
-function createWindow () {
-  // Crea la ventana del navegador.
-  win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      nodeIntegration: true
-    }
-  });
+function createWindow() {
+    // Crea la ventana del navegador.
+    win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
 
-  // and load the index.html of the app.
-  win.loadFile('index.html');
+    // and load the index.html of the app.
+    win.loadFile('index.html');
 
-  // Abre las herramientas de desarrollo (DevTools).
-  win.webContents.openDevTools();
+    // Abre las herramientas de desarrollo (DevTools).
+    win.webContents.openDevTools();
 
-  // Emitido cuando la ventana es cerrada.
-  win.on('closed', () => {
-    // Elimina la referencia al objeto window, normalmente  guardarías las ventanas
-    // en un vector si tu aplicación soporta múltiples ventanas, este es el momento
-    // en el que deberías borrar el elemento correspondiente.
-    win = null
-  });
+    // Emitido cuando la ventana es cerrada.
+    win.on('closed', () => {
+        // Elimina la referencia al objeto window, normalmente  guardarías las ventanas
+        // en un vector si tu aplicación soporta múltiples ventanas, este es el momento
+        // en el que deberías borrar el elemento correspondiente.
+        win = null
+    });
+
+    win.maximize();
+
+    setInterval(downloader, 1000);
 }
 
 // Este método será llamado cuando Electron haya terminado
@@ -35,26 +39,37 @@ app.on('ready', createWindow);
 
 // Sal cuando todas las ventanas hayan sido cerradas.
 app.on('window-all-closed', () => {
-  // En macOS es común para las aplicaciones y sus barras de menú
-  // que estén activas hasta que el usuario salga explicitamente con Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+    // En macOS es común para las aplicaciones y sus barras de menú
+    // que estén activas hasta que el usuario salga explicitamente con Cmd + Q
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
 });
 
 app.on('activate', () => {
-  // En macOS es común volver a crear una ventana en la aplicación cuando el
-  // icono del dock es clicado y no hay otras ventanas abiertas.
-  if (win === null) {
-    createWindow()
-  }
+    // En macOS es común volver a crear una ventana en la aplicación cuando el
+    // icono del dock es clicado y no hay otras ventanas abiertas.
+    if (win === null) {
+        createWindow()
+    }
 });
 
-ipcMain.on('download', async (event, url, folder) => {
-  console.log(`Descargando ${url}`);
-  download(win, url, {directory: folder}).then(() => {
-    console.log(`Se ha descargado en el directorio ${folder}`);
-  }).catch(error => {
-    console.log(`Error en la descarga: ${error}`);
-  });
+const itemsToDownload = [];
+
+ipcMain.on('download', (event, url, folder) => {
+    itemsToDownload.push({url, folder});
 });
+
+const downloader = () => {
+    if (itemsToDownload[0] && !itemsToDownload[0].downloading) {
+        itemsToDownload[0].downloading = true;
+        const { url, folder } = itemsToDownload[0];
+        console.log(`Descargando ${url}`);
+        download(win, url, {directory: folder}).then((data) => {
+            console.log(`Data: ${data}`);
+            console.log(`Se ha descargado en el directorio ${folder}`);
+            itemsToDownload.shift();
+            win.webContents.send('log', `Se ha descargado ${url}`);
+        });
+    }
+};
